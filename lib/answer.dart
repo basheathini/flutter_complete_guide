3f4fb6a900bb32018537283f997697ec7c3c458f
masterbashe <bashe.athini@gmail.com>
2019-07-24 16:21:13 +0200

    initial push
    layouts

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final Function handler;
 
-  Answer(this.handler);
+  final Function handler;
+  final String answer;
+
+  Answer(this.handler, this.answer);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: EdgeInsets.all(1),
       child: RaisedButton(
         onPressed: handler,
         color: Colors.blue,
-        child: Text("Answer 1"),
+        textColor: Colors.white,
+        child: Text(answer),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _index = 1;
+  var _index = 0;
 
   void _answerQuestion() {
     setState(() {
@@ -24,29 +24,36 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'what is your favorite color?',
-      'How many dogs do you have?',
-      'where do you live?'
-    ];
-    var answers = ['Red', 'I dont have any dog', 'I live at Rosebank'];
+
+//    var isTrue = false;
+//
+//    //isTrue ? print('base') : print('athini');
+//
+//    isTrue? print('true') : print('false');
+
+    final questions = [
+      {'question' : 'what is your favorite color?', 'answer' : ['Black', 'Black', 'Black']},
+      {'question' : 'How many dogs do you have?', 'answer' : ['1', '2', '3']},
+      {'question' : 'where do you live?', 'answer' : ['Port St Johns', 'East London', 'Port Elizabeth', 'Umtata', 'Bisho', 'King William\'s Town', 'Cape Town']}];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Scaffold"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_index]),
-            Text(answers[_index]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            RaisedButton(
-              child: Text('click me'),
-              onPressed: _answerQuestion,
-            ),
-          ],
+        body: Container(
+          margin: EdgeInsets.all(80),
+          child: Column(
+            children: <Widget>[
+              Question(questions[_index]['question']),
+
+              ...(questions[_index]['answer'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+
+            ],
+          ),
         ),
       ),
     );
